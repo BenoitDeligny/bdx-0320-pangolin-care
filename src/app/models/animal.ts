d4573409e6875e93e2eafc8482eb6f1729cc5151
Couettou <delignyb.pro@gmail.com>
2020-04-15 10:48:57 +0200

    clean Animal Class

--- src/app/models/animal.ts
+++ src/app/models/animal.ts
@@ -1,22 +1,17 @@
 export class Animal {
-    "taxonid": number;
-    "scientific_name": string;
-    "kingdom": string;
-    "phylum": string;
-    "class": string;
-    "order": string;
-    "family": string;
-    "genus": string;
-    "main_common_name": string;
-    "authority": string;
-    "published_year": number;
-    "assessment_date": string;
-    "category": string;
-    "criteria": string;
-    "population_trend": string;
-    "marine_system": boolean;
-    "freshwater_system": boolean;
-    "terrestrial_system": boolean;
-    "assessor": string;
-    "reviewer": string;
+    'taxonid': number;
+    'scientific_name': string;
+    'kingdom': string;
+    'phylum': string;
+    'class': string;
+    'order': string;
+    'family': string;
+    'genus': string;
+    'main_common_name': string;
+    'category': string;
+    'criteria': string;
+    'population_trend': string;
+    'marine_system': boolean;
+    'freshwater_system': boolean;
+    'terrestrial_system': boolean;
 }
