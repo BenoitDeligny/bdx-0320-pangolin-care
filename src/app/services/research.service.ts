377938f8787af60f71c7f4d9174ee662486955de
Couettou <delignyb.pro@gmail.com>
2020-04-15 11:05:42 +0200

    requête Http test mise dans le service

--- src/app/services/research.service.ts
+++ src/app/services/research.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Animal } from '../models/animal';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ResearchService {
 
-  constructor() { }
+  constructor(private http: HttpClientModule) { }
+
+
+  getAnimal(): Observable<Animal> {
+    return this.http.get<Animal>('https://apiv3.iucnredlist.org/api/v3/species/loxodonta%20africana?token=9bb4facb6d23f48efbf424bb05c0c1ef1cf6f468393bc745d42179ac4aca5fee');
+  }
 }
