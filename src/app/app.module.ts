bc3e14b1d98a3f3c36d04a0b12f171284c118409
Xavi <xaviregnier@gmail.com>
2020-04-15 10:01:44 +0200

    countrypage created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CountrypageComponent } from './countrypage/countrypage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CountrypageComponent
   ],
   imports: [
     BrowserModule,
