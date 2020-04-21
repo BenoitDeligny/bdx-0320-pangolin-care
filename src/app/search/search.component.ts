6097c62c43edcfb877a8f33714695e573ba03d4c
Couettou <delignyb.pro@gmail.com>
2020-04-21 15:02:58 +0200

    add comments on sendRequest() + list of species txt added

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -5,6 +5,7 @@ import { Country } from '../models/country-list';
 import { AnimalByCountryAnswer } from '../models/animal-by-country-answer';
 import { AnimalDetailsAnswer } from '../models/animaldetailsanswer';
 import { AnimalCountriesAnswer } from '../models/animal-countries-answer';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'pgc-search',
@@ -21,7 +22,7 @@ export class SearchComponent implements OnInit {
   countries: Country[] = [];
   animalsByCountry: Animal[] = [];
 
-  constructor(private researchService: ResearchService) { }
+  constructor(private researchService: ResearchService, private router: Router) { }
 
   ngOnInit(): void {
     this.researchService.getArrayOfCountries()
@@ -29,26 +30,30 @@ export class SearchComponent implements OnInit {
   }
 
   sendRequest() {
+
+    // Convert the user Input to ISOCODE
     const country = this.allCountries.find(currentElement => currentElement.country.toUpperCase() === this.criteria.toUpperCase());
     if (country === undefined) {
       alert('Pays inconnu');
       return;
     }
-
     // this.criteria = testing.isocode;
 
+    // Search for list of animal IN one Country
     this.researchService.getAnimalByCountry(country.isocode).subscribe(
       (animalByCountryFromServer: AnimalByCountryAnswer) => {
         this.animalsByCountry = animalByCountryFromServer.result;
       }
     );
 
+    // Search for Details of one Animal
     this.researchService.getAnimalDetails(country.isocode).subscribe(
       (animalFromServer: AnimalDetailsAnswer) => {
         this.animals = animalFromServer.result;
       }
     );
 
+    // Search for all Countries in one animal lives
     this.researchService.getAnimalCountries(country.isocode).subscribe(
       (countryListFromServer: AnimalCountriesAnswer) => {
         this.countries = countryListFromServer.result;
