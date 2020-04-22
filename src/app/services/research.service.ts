e46694553e2226bf31e6630a464b6ad06dd168b5
Couettou <delignyb.pro@gmail.com>
2020-04-22 10:20:16 +0200

    all fixes SOLVED

--- src/app/services/research.service.ts
+++ src/app/services/research.service.ts
@@ -21,10 +21,6 @@ export class ResearchService {
   BASE_URL = `https://apiv3.iucnredlist.org/api/v3/`;
   TOKEN = `?token=9bb4facb6d23f48efbf424bb05c0c1ef1cf6f468393bc745d42179ac4aca5fee`;
 
-  animalByCountryURL = '';
-  animalDetailsURL = '';
-  animalCountriesURL = '';
-  descriptionsURL = '';
 
   getAnimalByCountry(criteria: string): Observable<AnimalByCountryAnswer> {
     const url = this.BASE_URL + `country/getspecies/${criteria}` + this.TOKEN;
