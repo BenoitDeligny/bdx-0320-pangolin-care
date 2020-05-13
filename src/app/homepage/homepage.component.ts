ae8296de2246307c8c60e5624a1111e4c486ae7f
Couettou <delignyb.pro@gmail.com>
2020-05-13 12:24:39 +0200

    FIX homepage router delete

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -9,7 +9,7 @@ import { Router } from '@angular/router';
 export class HomepageComponent implements OnInit {
 
 
-  constructor(private router: Router) { }
+  constructor() { }
 
   ngOnInit(): void {
   }
